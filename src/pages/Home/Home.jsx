826b5035f9faa3f8c544fd94c977d4c8654deda6
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-07-25 10:20:50 +0200

    only card css design needed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 
+import {useEffect, useState} from 'react';
 import { CharacterCard } from '../../common/CharacterCard/CharacterCard';
 import { bringCharacters } from '../../services/apiCalls';
 import './Home.css';
