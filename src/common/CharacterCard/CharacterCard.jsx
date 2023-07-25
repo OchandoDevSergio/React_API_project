768b4fb6dfade815813f16a9084cacd1540002dd
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-07-25 10:05:24 +0200

    not proven but only left to make the css design for the cards

---
+++ src/common/CharacterCard/CharacterCard.jsx
@@ -0,0 +1,10 @@
+
+
+import './CharacterCard.css';
+
+export const CharacterCard = ({key, name, picture, status}) => {
+
+    return (
+        <>{name}</>
+    )
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,52 @@
+
+import { CharacterCard } from '../../common/CharacterCard/CharacterCard';
+import { bringCharacters } from '../../services/apiCalls';
+import './Home.css';
+
+export const Home = () => {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(()=>{
+
+        if(characters.length === 0){
+            //Al no haber ningún personaje aún de la API de rick and morty, lo más
+            //correcto sería traerlos...
+          bringCharacters()
+                .then(
+                    resultado => {
+                        setCharacters(resultado.data.results);
+                    }
+                )
+                .catch(error => console.log(error));
+        } else {
+            console.log(characters);
+        }
+    },[characters]);
+
+    return (
+        <>
+            {characters.length > 0 
+            
+                ? (<div className='homeDesign'>
+                    {characters.map(
+                        person => {
+                            return (
+                                <CharacterCard
+                                
+                                    key={person.id}
+                                    name={person.name}
+                                    picture={person.image}
+                                    status={person.status}
+                                    
+                                />
+                            )
+                        }
+                    )}
+                </div>)
+
+                : (<div>en camino</div>)
+            
+            }
+        </>
+    )
+}
